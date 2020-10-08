8c5a86c62b19383eca3f83fff9eee05f6bf137a5
André Bernardino <andre.bernardino@tecnico.ulisboa.pt>
2020-10-08 22:51:04 +0100

    Add unit test for inappropriate words

--- src/test/java/com/github/reducktion/socrates/validator/MexicoValidatorTest.java
+++ src/test/java/com/github/reducktion/socrates/validator/MexicoValidatorTest.java
@@ -33,6 +33,53 @@ class MexicoValidatorTest {
 
     @ParameterizedTest
     @ValueSource(strings = {
+        "BUEI000000AAAAAA01",
+        "CACA000000AAAAAA01",
+        "CAGA000000AAAAAA01",
+        "CAKA000000AAAAAA01",
+        "COGE000000AAAAAA01",
+        "COJE000000AAAAAA01",
+        "COJO000000AAAAAA01",
+        "FETO000000AAAAAA01",
+        "JOTO000000AAAAAA01",
+        "KACO000000AAAAAA01",
+        "KAGO000000AAAAAA01",
+        "KOJO000000AAAAAA01",
+        "KULO000000AAAAAA01",
+        "MAMO000000AAAAAA01",
+        "MEAS000000AAAAAA01",
+        "MION000000AAAAAA01",
+        "MULA000000AAAAAA01",
+        "PEDO000000AAAAAA01",
+        "PUTA000000AAAAAA01",
+        "RUIN000000AAAAAA01",
+        "BUEY000000AAAAAA01",
+        "CACO000000AAAAAA01",
+        "CAGO000000AAAAAA01",
+        "CAKO000000AAAAAA01",
+        "COJA000000AAAAAA01",
+        "COJI000000AAAAAA01",
+        "CULO000000AAAAAA01",
+        "GUEY000000AAAAAA01",
+        "KACA000000AAAAAA01",
+        "KAGA000000AAAAAA01",
+        "KOGE000000AAAAAA01",
+        "KAKA000000AAAAAA01",
+        "MAME000000AAAAAA01",
+        "MEAR000000AAAAAA01",
+        "MEON000000AAAAAA01",
+        "MOCO000000AAAAAA01",
+        "PEDA000000AAAAAA01",
+        "PENE000000AAAAAA01",
+        "PUTO000000AAAAAA01",
+        "RATA000000AAAAAA01",
+    })
+    void validate_shouldReturnFalse_whenIdContainsInappropriateWord(final String idWithInappropriateWord) {
+        assertThat(mexicoIdValidator.validate(idWithInappropriateWord), is(false));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
         " AAIM901112MBCNMN08 ",
         "JOIM890106HHGSMN08",
         "JOTA950616HBCSWS03",
