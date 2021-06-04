d6117238c362df1155cbebeed6651002d1ac92ef
afbernardino <11179175+afbernardino@users.noreply.github.com>
2021-06-04 14:50:57 +0100

    Fix unit test

--- src/test/java/com/github/reducktion/socrates/nationalid/NationalIdFactoryTest.java
+++ src/test/java/com/github/reducktion/socrates/nationalid/NationalIdFactoryTest.java
@@ -30,7 +30,7 @@ class NationalIdFactoryTest {
             Arguments.arguments(Country.CA, CanadaNationalId.class),
             Arguments.arguments(Country.DE, GermanyNationalId.class),
             Arguments.arguments(Country.DK, DenmarkNationalId.class),
-            Arguments.arguments(Country.ES, SpainNationalIdTest.class),
+            Arguments.arguments(Country.ES, SpainNationalId.class),
             Arguments.arguments(Country.FR, FranceNationalId.class),
             Arguments.arguments(Country.IT, ItalyNationalId.class),
             Arguments.arguments(Country.LU, LuxembourgNationalId.class),
