011915de779a5c3bc72fa6d86d79f3df8541d6c4
afbernardino <11179175+afbernardino@users.noreply.github.com>
2021-06-04 10:37:33 +0100

    Deprecate class PortugalNationalId

--- src/main/java/com/github/reducktion/socrates/validator/PortugalIdValidator.java
+++ src/main/java/com/github/reducktion/socrates/validator/PortugalIdValidator.java
@@ -7,7 +7,10 @@ import com.github.reducktion.socrates.internal.LuhnAlgorithm;
  *
  * This validation algorithm is based on the official documentation released in 26 of January of 2009:
  * https://www.autenticacao.gov.pt/documents/20126/115760/Valida%C3%A7%C3%A3o+de+N%C3%BAmero+de+Documento+do+Cart%C3%A3o+de+Cidad%C3%A3o.pdf/bdc4eb37-7316-3ff4-164a-f869382b7053
- **/
+ *
+ * @deprecated Use class PortugalNationalId instead
+ */
+@Deprecated
 class PortugalIdValidator implements IdValidator {
 
     private static final int ID_NUMBER_OF_CHARACTERS = 12;
