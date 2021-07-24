ca8f09b13b13cd7e5aa833b7270bc7e3272c9068
afbernardino <andre.bernardino@tecnico.ulisboa.pt>
2021-07-24 13:20:45 +0100

    Remove deprecated classes and methods

--- src/main/java/com/github/reducktion/socrates/nationalid/NationalIdFactory.java
+++ src/main/java/com/github/reducktion/socrates/nationalid/NationalIdFactory.java
@@ -9,22 +9,6 @@ public final class NationalIdFactory {
     /**
      * Returns a new instance of {@link NationalId}, that is specific for the country parameter.
      *
-     * @deprecated use {@link #newInstance(String, Country)} instead
-     *
-     * @param id the national id
-     * @param country the {@link Country}
-     * @return a new instance of {@link NationalId}
-     * @throws UnsupportedOperationException if the country is not supported
-     *
-     */
-    @Deprecated
-    public static NationalId getNationalId(final String id, final Country country) {
-        return newInstance(id, country);
-    }
-
-    /**
-     * Returns a new instance of {@link NationalId}, that is specific for the country parameter.
-     *
      * @param id the national id
      * @param country the {@link Country}
      * @return a new instance of {@link NationalId}
